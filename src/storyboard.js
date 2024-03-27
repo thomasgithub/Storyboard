e04e4e0479d19554de2cc12083c83cc43cedeafa
rfriscca <remifriscca@gmail.com>
2024-03-27 10:17:50 +0100

    thomasgithub/storyplayer-server#4426

--- src/storyboard.js
+++ src/storyboard.js
@@ -222,6 +222,16 @@ class Storyboard {
     }
   }
 
+  /**
+   * Go to specified index
+   */
+  goto (index) {
+    if (index >= 0 && index < this._images.length) {
+      this._index = index;
+      this.enableAnim();
+      this.refresh();
+    }
+  }
 }
 
 module.exports = Storyboard
